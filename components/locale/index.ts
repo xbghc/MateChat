43882ae14fa6cb2fef5edc71f3c4f3ec38ac14e7
gellenliu <1042382110@qq.com>
2025-03-03 19:46:42 +0800

    feat: 支持组件国际化

---
+++ components/locale/index.ts
@@ -0,0 +1,26 @@
+import type { App } from 'vue';
+import { createMcI18n } from './i18n';
+import enUs from './lang/en-us';
+import zhCn from './lang/zh-cn';
+
+const McDefaultMesaages = {
+  'en-us': enUs,
+  'zh-cn': zhCn,
+};
+
+const McI18n = createMcI18n({
+  locale: 'zh-cn',
+  messages: McDefaultMesaages,
+});
+
+export function useMcI18n() {
+  return McI18n;
+}
+
+export const McLocale = {
+  install: (app: App) => {
+    app.config.globalProperties.McI18n = McI18n;
+  }
+};
+
+export default McI18n;
---
+++ components/locale/lang/en-us.ts
@@ -0,0 +1,8 @@
+export default {
+  Input: {
+    send: 'Send',
+    pauseAnswer: 'Pause Answering',
+    pleaseEnter: 'Please enter your question...',
+    pleaseEnterPlaceholder: 'Please enter your question and press {enterKey} to send, {shiftEnterKey} to wrap.',
+  },
+};
